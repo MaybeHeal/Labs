b9bdfee504ab1818cc9edd0e9f5fba59165b7c45
MaybeHeal <egich001@mail.ru>
2023-12-01 14:52:11 +0300

    Lab 4 complete

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -79,17 +79,105 @@ class Person:
     def person_qualification(self, count):
         self.__qualification = count
 
+    def fire_person(self):
+        print(self.__first_name, self.__second_name, 'вы уволены')
+        self.__del__()
 
-# def persons():
-#     person1 = Person('Вася', 'Пупкин', 3)
-#     person2 = Person('Гадя', 'Петрович', 1)
-#     person3 = Person('Олег', 'Молодой', 2)
-#     person_list = [person1.person_qualification, person2.person_qualification, person3.person_qualification]
-#     print(min(enumerate(person_list)))
-#     input()
+
+def persons():
+    person1 = Person('Вася', 'Пупкин', 3)
+    person2 = Person('Гадя', 'Петрович', 1)
+    person3 = Person('Олег', 'Иванов', 2)
+    person_list = [person1, person2, person3]
+    person_qualification_list = []
+    for person in person_list:
+        person_qualification_list.append(person.person_qualification)
+    for key, qualification in enumerate(person_qualification_list):
+        if qualification == min(person_qualification_list):
+            person_list[key].fire_person()
+    input('Нажмите Enter для продолжения')
+
+
+# Задание 4:
+class TriangleChecker:
+    def __init__(self):
+        self.__side_a = 0
+        self.__side_b = 0
+        self.__side_c = 0
+
+    def is_triangle(self):
+        ab_sum = self.__side_a + self.__side_b
+        bc_sum = self.__side_b + self.__side_c
+        ca_sum = self.__side_c + self.__side_c
+        if ab_sum > self.__side_c and bc_sum > self.__side_a and ca_sum > self.__side_b:
+            print('Ура, можно построить треугольник!')
+        else:
+            print('Жаль, но из этого треугольник не сделать.')
+
+    @property
+    def side_a_value(self):
+        return self.__side_a
+
+    @side_a_value.setter
+    def side_a_value(self, count):
+        while True:
+            try:
+                if float(count) < 0:
+                    print('С отрицательными числами ничего не выйдет!')
+                else:
+                    self.__side_a = float(count)
+                    break
+            except:
+                print('Нужно вводить только числа!')
+            count = input('Введите длину стороны a: ')
+
+    @property
+    def side_b_value(self):
+        return self.__side_b
+
+    @side_b_value.setter
+    def side_b_value(self, count):
+        while True:
+            try:
+                if float(count) < 0:
+                    print('С отрицательными числами ничего не выйдет!')
+                else:
+                    self.__side_b = float(count)
+                    break
+            except:
+                print('Нужно вводить только числа!')
+            count = input('Введите длину стороны b: ')
+
+    @property
+    def side_c_value(self):
+        return self.__side_c
+
+    @side_c_value.setter
+    def side_c_value(self, count):
+        while True:
+            try:
+                if float(count) < 0:
+                    print('С отрицательными числами ничего не выйдет!')
+                else:
+                    self.__side_c = float(count)
+                    break
+            except:
+                print('Нужно вводить только числа!')
+            count = input('Введите длину стороны c: ')
+
+
+def dose_a_triangle_exist():
+    triangle = TriangleChecker()
+
+    triangle.side_a_value = input('Введите длину стороны a: ')
+    triangle.side_b_value = input('Введите длину стороны b: ')
+    triangle.side_c_value = input('Введите длину стороны c: ')
+
+    triangle.is_triangle()
 
 
 if __name__ == '__main__':
     give_me_a_soda()
     warrior_fight()
-    # persons()
+    persons()
+    dose_a_triangle_exist()
